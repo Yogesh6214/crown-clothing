e1e413d1323e9c5fe3a68a472853c7b11c4afda5
Yogesh Kumar <globalaudios736@gmail.com>
2024-04-27 12:44:39 +0530

    Install the react router and Route the pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.components";
+
+const HatesPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1> Home Page</h1>
+    </div>
+  );
+};
+
+const TopicsList = () => {
+  return (
+    <div>
+      <h1> Topics List Page</h1>
+    </div>
+  );
+};
+
+const TopicsDetails = () => {
+  return (
+    <div>
+      <h1> Topics Details Page</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" Component={HomePage} />
+        <Route exact path="/hats" Component={HatesPage} />
+      </Routes>
     </div>
   );
 }
