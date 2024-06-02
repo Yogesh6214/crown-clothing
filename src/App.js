fb1761f1c33d952168cdf6469c4563055e230bf2
Yogesh Kumar <globalaudios736@gmail.com>
2024-06-02 22:38:13 +0530

    Create Shop Page

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.components";
-
-const HatesPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1> Hats Page</h1>
-    </div>
-  );
-};
-
-const TopicsList = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1> Topics List Page</h1>
-    </div>
-  );
-};
-
-const TopicsDetails = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1> Topics Details Page: {props.match.params.topicID}</h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/hats" element={<HatesPage />} />
-        <Route exact path="/topics" element={<TopicsList />} />
-        <Route exact path="/topics/:topicID" element={<TopicsDetails />} />
+        <Route exact path="/shop" element={<ShopPage />} />
       </Routes>
     </BrowserRouter>
   );
